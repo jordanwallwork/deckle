d1a6fb11d710caf94a3b7d729c53b9ff232d87b9
Jordan Wallwork <jrwallwork@gmail.com>
2026-01-08 22:34:20 +0000

    Add docker compose + basic github workflow

--- src/Deckle.AppHost/AppHost.cs
+++ src/Deckle.AppHost/AppHost.cs
@@ -1,9 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add a Docker Compose environment
+var compose = builder.AddDockerComposeEnvironment("compose");
+
 // Add PostgreSQL with pgAdmin and persistent data volume
 var postgres = builder.AddPostgres("postgres")
     .WithDataVolume()
-    .WithLifetime(ContainerLifetime.Persistent);
+    .WithLifetime(ContainerLifetime.Persistent)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "db-server";
+    });
 
 // Configure pgAdmin with health check
 postgres.WithPgAdmin(configureContainer: container => container
@@ -14,13 +21,21 @@ var database = postgres.AddDatabase("deckledb");
 var web = builder.AddNpmApp("web", "../Deckle.Web")
     .WithHttpEndpoint(port: 5173, env: "PORT")
     .WithExternalHttpEndpoints()
-    .PublishAsDockerFile();
+    .PublishAsDockerFile()
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "web";
+    });
 
 var api = builder.AddProject<Projects.Deckle_API>("api")
     .WithReference(database)
     .WithReference(web)
     .WithEnvironment("FrontendUrl", web.GetEndpoint("http"))
-    .WaitFor(database);
+    .WaitFor(database)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "api";
+    });
 
 web.WithReference(api)
     .WithEnvironment("PUBLIC_API_URL", api.GetEndpoint("http"));
